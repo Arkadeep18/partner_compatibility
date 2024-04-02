5bb9cb325ba91d887fdf2ef8aac095af606f39c9
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-04-02 10:38:58 +0530

    Add files via upload

---
+++ Marriage_compatibility.java
@@ -0,0 +1,135 @@
+import java.util.*;
+public class Marriage_compatibility {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your zodiac sign");
+		String My_zdc= sc.next().toLowerCase();
+		System.out.println("Enter your partner's zodiac sign");
+		String Partner_zdc = sc.next().toLowerCase();
+
+		//		String Aries = "Aries";
+		//		String Taurus = "Taurus";
+		//		String Gemini = "Gemini";
+		//		String Cancer = "Cancer";
+		//		String Leo = "Leo";
+		//		String Virgo = "Virgo";
+		//		String Libra = "Libra";
+		//		String Scorpio = "Scorpio";
+		//		String Sagittarius = "Sagittarius";
+		//		String Capricorn = "Capricorn";
+		//		String Aquarius = "Aquarius";
+		//		String Pisces = "Pisces";
+				
+
+		switch(My_zdc) {
+		case "aries": {
+			if(Partner_zdc.equals("Gemini")||Partner_zdc.equals("sagittarious")||Partner_zdc.equals("leo")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+ "You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "taurus": {
+			if(Partner_zdc.equals("cancer")||Partner_zdc.equals("pisces")||Partner_zdc.equals("capricorn")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+ "You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "gemini": {
+			if(Partner_zdc.equals("gemini")||Partner_zdc.equals("aquarius")||Partner_zdc.equals("libra")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+ "You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "cancer": {
+			if(Partner_zdc.equals("taurus")||Partner_zdc.equals("scorpio")||Partner_zdc.equals("virgo")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "leo": {
+			if(Partner_zdc.equals("sagittarious")||Partner_zdc.equals("aries")||Partner_zdc.equals("gemini")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "virgo": {
+			if(Partner_zdc.equals("capricorn")||Partner_zdc.equals("cancer")||Partner_zdc.equals("taurus")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "libra": {
+			if(Partner_zdc.equals("sagittarious")||Partner_zdc.equals("gemini")||Partner_zdc.equals("aquarius")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "scorpio": {
+			if(Partner_zdc.equals("scorpio")||Partner_zdc.equals("capricorn")||Partner_zdc.equals("pisces")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "sagittarious":
+			if(Partner_zdc.equals("aries")||Partner_zdc.equals("aquarius")||Partner_zdc.equals("sagittarious")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+		case "capricorn": {
+			if(Partner_zdc.equals("capricorn")||Partner_zdc.equals("pisces")||Partner_zdc.equals("taurus")||Partner_zdc.equals("virgo")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "pisces": {
+			if(Partner_zdc.equals("scorpio")||Partner_zdc.equals("cancer")||Partner_zdc.equals("capricorn")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond.");
+			}
+			break;
+		}
+		case "aquarius": {
+			if(Partner_zdc.equals("sagittarious")||Partner_zdc.equals("libra")||Partner_zdc.equals("gemini")) {
+				System.out.println("The compatibility between you two is remarkable, making every interaction between you both feel like a seamless blend of harmony and understanding");
+			}else {
+				System.out.println("There might be some room for improvement in your relationship. Taking time to communicate openly, understand each other's perspectives, and find common ground could be beneficial.\r\\n"
+						+"You both have the capability to overcome any challenges, just trust in yourselves and your bond."
+						);
+			}
+			break;
+			}default:System.out.println("Enter a valid zodiac sign. Your determination will guide you through any obstacles");
+		}
+	}
+}
